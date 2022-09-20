003748268fe117cd40c7b7f0bf49bc422b31e8c7
LStaley <lstaley9185@gmail.com>
2022-09-20 14:29:42 -0400

    Finished

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,43 @@
 public class Main
 {
-    public static void main(String[] args)
-    {
-        System.out.println("Hello world!");
+    public static void main(String[] args) {
+        int age = 16;
+
+        if (age. = 16) {
+            System.out.println("You can get a Drivers License");
+        }
+        if (age. = 16) {
+            System.out.println("You can get a Drivers License");
+        } else {
+            System.out.println("Sorry too young ofr a Driver's license");
+        }
+
+        // Cascaded if then else used for selection
+
+        String playerA = "R";
+        String playerB = "R";
+
+        if(playerA.equals("R"))
+        {
+            System.out.println("Move is Rock");
+        }
+        else if (playerA.equals("P"))
+        {
+            System.out.println("Move is Paper");
+        }
+        else
+        {
+            System.out.println("Has to be Scissors");
+        }
+        /* == Example / explanation
+        String name = "Tom";
+        String fname = name;
+
+        if (name.equals(fname))
+        {
+            System.out.println("Same Variable");
+        }
+         */
+
     }
 }
